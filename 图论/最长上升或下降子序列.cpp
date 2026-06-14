8ef1b3b4f2b303c5bfae82bdee6d2f2afcd5e5e1
Jerry Li <lizhangrui2012@gmail.com>
2026-06-14 11:35:58 +0800

    算法模版

--- 图论/最长上升或下降子序列.cpp
+++ 图论/最长上升或下降子序列.cpp
@@ -6,15 +6,18 @@ int n;
 int a[1010];//存储数组
 int main(){
     cin>>n;
-    for (int i=1;i<=n;i++) {
-    dp[i]=1;
-    for (int j=1;j<i;j++) {//上升
-        if (a[j]<a[i]) {//说明a[i]可以a[j]后面
-            dp[i]=max(dp[i],dp[j]+1);//既然a[j]后面能接a[i]，
-            //那“以j结尾的最长上升子序列”长度再加1,就是一种以i结尾的方案。
-        }
+    for(int i=1;i<=n;i++){
+        cin>>a[i];
     }
-    ans = max(ans, dp[i]);
+    for (int i=1;i<=n;i++) {
+        dp[i]=1;
+        for (int j=1;j<i;j++) {//上升
+            if (a[j]<a[i]) {//说明a[i]可以a[j]后面
+                dp[i]=max(dp[i],dp[j]+1);//既然a[j]后面能接a[i]，
+                //那“以j结尾的最长上升子序列”长度再加1,就是一种以i结尾的方案。
+            }
+        }
+        ans = max(ans, dp[i]);
     }
     /*for (int i=1;i<=n;i++) {//下降
         dp[i]=1;
@@ -25,5 +28,6 @@ int main(){
         }
         ans=max(ans,dp[i]);
     }*/
+   cout<<ans<<endl;
     return 0;
 }
